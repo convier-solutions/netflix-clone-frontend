e44ddb88dc263503e779b723117e24339b2bd899
Aswad Kaleem <141782449+O-OBAID@users.noreply.github.com>
2024-10-25 18:34:49 +0500

    Updated text field container

--- src/components/common/TextField/TextField.jsx
+++ src/components/common/TextField/TextField.jsx
@@ -4,10 +4,8 @@ import './TextField.css';
 const InputField = ({ type = 'text', value, onChange, error, placeholder, maxWidth }) => {
   return (
     <div className="input-container">
-      <div>
-        <input type={type} value={value} onChange={onChange} placeholder={placeholder} style={{ maxWidth }} className={`input-field ${error ? 'error' : ''}`} />
-        {error && <span className="error-message">{error}</span>}
-      </div>
+      <input type={type} value={value} onChange={onChange} placeholder={placeholder} style={{ maxWidth }} className={`input-field ${error ? 'error' : ''}`} />
+      {error && <span className="error-message">{error}</span>}
     </div>
   );
 };
